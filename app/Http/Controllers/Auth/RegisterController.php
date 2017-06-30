8a3795323cdeefb50188a4ab46599eebd336a08e
sald19 <carl.sald19@gmail.com>
2017-06-29 18:56:53 -0600

    change redirect to

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,4 +68,9 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+
+    public function redirectTo()
+    {
+        return auth()->user()->leagues->isEmpty() ? '/league/create' : '/league/index';
+    }
 }
